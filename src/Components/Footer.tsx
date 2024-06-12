31690e989a3df8daea3a3324e231b6bb3da91ef0
ThatWeirdLady <ThatWeirdLady@gmail.com>
2024-06-12 16:39:25 -0400

    remove unused components

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,27 +1,20 @@
-import React from 'react'
-import { grey } from "../index"
-import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import patreon from '../images/icons8-patreon-50.png';
-
+import React from "react";
+import { grey } from "../index";
+import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import patreon from "../images/icons8-patreon-50.png";
 
 const Footer = () => {
-    return (
-        <Box sx={{ backgroundColor: grey, height: "600", width: "100%" }}>
-            <BottomNavigation sx={{ width: "100%", backgroundColor: "#00000000" }}>
-                <BottomNavigationAction
-                    icon={<YouTubeIcon />}
-                />
-                <BottomNavigationAction
-                    icon={<FacebookIcon />}
-                />
-                <BottomNavigationAction sx={{}}
-                    icon={<img src={patreon} />}
-                />
-            </BottomNavigation>
-        </Box>
-    )
-}
+  return (
+    <Box sx={{ backgroundColor: grey, height: "600", width: "100%" }}>
+      <BottomNavigation sx={{ width: "100%", backgroundColor: "#00000000" }}>
+        <BottomNavigationAction icon={<YouTubeIcon />} />
+        <BottomNavigationAction icon={<FacebookIcon />} />
+        <BottomNavigationAction sx={{}} icon={<img src={patreon} alt="" />} />
+      </BottomNavigation>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
